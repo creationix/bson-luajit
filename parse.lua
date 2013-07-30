770bea54a3550ecda6e88f8629505e2b2bd0fdc7
Tim Caswell <tim@creationix.com>
2013-07-30 11:06:24 -0500

    Cleanup parser

--- parse.lua
+++ parse.lua
@@ -1,81 +1,12 @@
 local prettyPrint = require("utils").prettyPrint
 local ffi = require 'ffi'
 
-local types, UInt32, Double, String, Document, Array, Binary, Undefined,
-      ObjectID, Boolean, DateTime, Null, CString, DBPointer, JavaScript,
-      Symbol, ScopedJavaScript, Int32, TimeStamp, Int64, Max, Min
-
-function UInt32(buf, i)
-  return ffi.cast("uint32_t*", buf + i)[0], 4
-end
+local types, Double, String, Document, Array, Binary, Undefined, ObjectID,
+      Boolean, DateTime, Null, RegularExpression, DBPointer, JavaScript, Symbol,
+      ScopedJavaScript, Int32, TimeStamp, Int64, Max, Min
 
 function Double(buf, i)
   return ffi.cast("double*", buf + i)[0], 8
-end
-
-function String(buf, i)
-  local len, consumed = UInt32(buf, i)
-  return ffi.string(buf + i + consumed, len - 1), len + consumed
-end
-
-function Document(buf, i)
-  local length, consumed, last, doc, parser, name
-  length, consumed = UInt32(buf, i)
-  last = i + length - 1
-  i = i + consumed
-  doc = {}
-  while i < last do
-    parser = assert(types[buf[i]], "Invalid entry type: " .. buf[i])
-    i = i + 1
-    name, consumed = CString(buf, i)
-    i = i + consumed
-    doc[name] ,consumed = parser(buf, i)
-    i = i + consumed
-  end
-  return doc, length
-end
-
-function Array(buf, i)
-  local length, consumed, last, doc, parser, index
-  length, consumed = UInt32(buf, i)
-  last = i + length - 1
-  i = i + consumed
-  doc = {}
-  while i < last do
-    parser = assert(types[buf[i]], "Invalid entry type: " .. buf[i])
-    i = i + 1
-    index = buf[i] - 48
-    assert(index == #doc, "Array keys must increment")
-    assert(buf[i + 1] == 0, "Array key must be null terminated")
-    i = i + 2
-    doc[index + 1], consumed = parser(buf, i)
-    i = i + consumed
-  end
-  return doc, length
-end
-
-function Binary(buf, i)
-  error "TODO: Implement Binary"
-end
-
-function Undefined(buf, i)
-  error "TODO: Implement Undefined"
-end
-
-function ObjectID(buf, i)
-  error "TODO: Implement ObjectID"
-end
-
-function Boolean(buf, i)
-  error "TODO: Implement Boolean"
-end
-
-function DateTime(buf, i)
-  error "TODO: Implement DateTime"
-end
-
-function Null(buf, i)
-  error "TODO: Implement Null"
 end
 
 function CString(buf, i)
@@ -83,20 +14,119 @@ function CString(buf, i)
   return string, #string + 1
 end
 
+function String(buf, i)
+  local length = ffi.cast("uint32_t*", buf + i)[0]
+  return ffi.string(buf + i + 4, length - 1), length + 4
+end
+
+function Document(buf, i)
+  local length = ffi.cast("uint32_t*", buf + i)[0]
+  local last = i + length - 1
+  i = i + 4
+  local doc = {}
+  while i < last do
+    local parser = assert(types[buf[i]], "Invalid entry type: " .. buf[i])
+    i = i + 1
+    local name = ffi.string(buf + i)
+    i = i + #name + 1
+    local consumed
+    doc[name], consumed = parser(buf, i)
+    i = i + consumed
+  end
+  return doc, length
+end
+
+function Array(buf, i)
+  local length = ffi.cast("uint32_t*", buf + i)[0]
+  local last = i + length - 1
+  i = i + 4
+  local doc = {}
+  while i < last do
+    local parser = assert(types[buf[i]], "Invalid entry type: " .. buf[i])
+    i = i + 1
+    local index = buf[i] - 48
+    assert(index == #doc, "Array keys must increment")
+    assert(buf[i + 1] == 0, "Array key must be null terminated")
+    i = i + 2
+    local consumed
+    doc[index + 1], consumed = parser(buf, i)
+    i = i + consumed
+  end
+  return doc, length
+end
+
+function Binary(buf, i)
+  local length = ffi.cast("uint32_t*", buf + i)[0]
+  return {
+    subtype = buf[i + 4],
+    data = ffi.string(buf + i + 5, length)
+  }, length + 5
+end
+
+function Undefined(buf, i)
+  return nil, 0
+end
+
+function ObjectID(buf, i)
+  return ffi.string(buf + i, 12), 12
+end
+
+function Boolean(buf, i)
+  return buf[i] > 0 and true or false, 1
+end
+
+function DateTime(buf, i)
+  return ffi.cast("uint64_t*", buf + i)[0], 8
+end
+
+function Null(buf, i)
+  return nil, 0
+end
+
+function RegularExpression(buf, i)
+  local start = i
+  local pattern = ffi.string(buf + i)
+  i = i + #pattern + 1
+  local options = ffi.string(buf + i)
+  i = i + #options + 1
+  return {
+    pattern = pattern,
+    options = options
+  }, i - start
+end
+
 function DBPointer(buf, i)
-  error "TODO: Implement DBPointer"
+  local start = i
+  local name = ffi.string(buf + i)
+  i = i + #name + 1
+  local id = ffi.string(buf + i, 12)
+  i = i + 12
+  return {
+    name = name,
+    id = id
+  }, i - start
 end
 
 function JavaScript(buf, i)
-  error "TODO: Implement JavaScript"
+  local js = ffi.string(buf + i)
+  return js, #js + 1
 end
 
 function Symbol(buf, i)
-  error "TODO: Implement Symbol"
+  local symbol = ffi.string(buf + i)
+  return symbol, #symbol + 1
 end
 
 function ScopedJavaScript(buf, i)
-  error "TODO: Implement ScopedJavaScript"
+  local start = i
+  local js = ffi.string(buf + i)
+  i = i + #js + 1
+  local scope, consumed = Document(buf, i)
+  i = i + consumed
+  return {
+    js = js,
+    scope = scope
+  }, i - start
 end
 
 function Int32(buf, i)
@@ -104,7 +134,7 @@ function Int32(buf, i)
 end
 
 function TimeStamp(buf, i)
-  error "TODO: Implement TimeStamp"
+  return ffi.cast("uint64_t*", buf + i)[0], 8
 end
 
 function Int64(buf, i)
@@ -112,11 +142,11 @@ function Int64(buf, i)
 end
 
 function Max(buf, i)
-  error "TODO: Implement Max"
+  return math.huge, 0
 end
 
 function Min(buf, i)
-  error "TODO: Implement Min"
+  return -math.huge, 0
 end
 
 types = {
@@ -130,7 +160,7 @@ types = {
   [8] = Boolean,
   [9] = DateTime,
   [10] = Null,
-  [11] = CString,
+  [11] = RegularExpression,
   [12] = DBPointer,
   [13] = JavaScript,
   [14] = Symbol,
